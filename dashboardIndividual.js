e1ef8067c84eb462dfd24d0da0bd9d79ea6d26bc
tomas9silva <tomas9silva@gmail.com>
2026-07-10 15:54:07 +0100

    Delete dashboardIndividual.js

--- dashboardIndividual.js
+++
@@ -1,5 +0,0 @@
-/*
-Ficheiro reservado para alterações futuras ao Dashboard Individual. Carrega depois de app.js, por isso pode sobrescrever funções sem mexer no ficheiro principal.
-
-Nesta primeira fase, a lógica original continua em ../app.js para garantir que nada parte.
-*/
