332e2450ca64ed7e661c9c465ae97c41a5d7e4fe
tomas9silva <tomas9silva@gmail.com>
2026-07-10 15:52:04 +0100

    Delete ui.js

--- ui.js
+++
@@ -1,5 +0,0 @@
-/*
-Ficheiro reservado para alterações futuras à interface, menus, modais e navegação.
-
-Nesta primeira fase, a lógica original continua em ../app.js para garantir que nada parte.
-*/
