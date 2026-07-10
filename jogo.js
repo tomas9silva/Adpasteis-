6152f4f13633b07c8fa2fb7900525b7c0c7b0500
tomas9silva <tomas9silva@gmail.com>
2026-07-10 15:54:28 +0100

    Delete jogo.js

--- jogo.js
+++
@@ -1,5 +0,0 @@
-/*
-Ficheiro reservado para alterações futuras ao marcador, cronómetro, arena e lógica de jogo.
-
-Nesta primeira fase, a lógica original continua em ../app.js para garantir que nada parte.
-*/
